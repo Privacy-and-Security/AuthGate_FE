3963d9d9325ea5cb2bc066a175c37b5d1f837991
Zhongyi-Lu <lu.zhon@northeastern.edu>
2023-11-05 15:43:46 -0800

    add default

--- src/pages/auth/jwt/index.js
+++ src/pages/auth/jwt/index.js
@@ -1,2 +1,13 @@
 export { default as JwtLoginView } from './jwt-login-view';
 export { default as JwtRegisterView } from './jwt-register-view';
+
+import React from 'react';
+
+const Jwt = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Jwt;
--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -1,3 +1,14 @@
 export { default as View403 } from './403-view';
 export { default as View500 } from './500-view';
 export { default as NotFoundView } from './not-found-view';
+
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Error;
