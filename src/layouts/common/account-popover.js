15416a34d92daa3534b27c19fa683c1936027285
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-11-07 21:02:11 -0800

    feat: real user email in account-popover

--- src/layouts/common/account-popover.js
+++ src/layouts/common/account-popover.js
@@ -11,16 +11,18 @@ import Typography from '@mui/material/Typography';
 import { useRouter } from 'src/routes/hooks';
 
 import { useMockedUser } from 'src/hooks/use-mocked-user';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 import { varHover } from 'src/components/animate';
 import CustomPopover, { usePopover } from 'src/components/custom-popover';
+import { _mock } from 'src/_mock';
 
 // ----------------------------------------------------------------------
 
 export default function AccountPopover() {
   const router = useRouter();
 
-  const { user } = useMockedUser();
+  const { user, error, isLoading } = useUser();
 
   const popover = usePopover();
 
@@ -57,26 +59,26 @@ export default function AccountPopover() {
         }}
       >
         <Avatar
-          src={user?.photoURL}
-          alt={user?.displayName}
+          src={_mock.image.avatar(24)}
+          alt={user.email}
           sx={{
             width: 36,
             height: 36,
             border: (theme) => `solid 2px ${theme.palette.background.default}`,
           }}
         >
-          {user?.displayName.charAt(0).toUpperCase()}
+          {user.email.charAt(0).toUpperCase()}
         </Avatar>
       </IconButton>
 
       <CustomPopover open={popover.open} onClose={popover.onClose} sx={{ width: 200, p: 0 }}>
         <Box sx={{ p: 2, pb: 1.5 }}>
           <Typography variant="subtitle2" noWrap>
-            {user?.displayName}
+            Welcome
           </Typography>
 
           <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
-            {user?.email}
+            {user.email}
           </Typography>
         </Box>
 
